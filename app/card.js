7b29ba7a80f76b719183a397752e018174b60235
yukendhiran <yukendhiran4@gmail.com>
2023-08-25 15:40:41 +0530

    minor fix v0.4

--- app/card.js
+++ app/card.js
@@ -3,10 +3,10 @@ export default function Card(props) {
 	const detailsList = props.details.split('- ').filter((item) => item.trim() !== '');
   
 	return (
-	  <div className='bg-black p-6 rounded-lg shadow-lg h-3/6 sm:w-3/4 mx-auto lg:w-full'>
+	  <div className='bg-black p-6 rounded-lg shadow-lg h-3/6 w-full mx-auto w-full'>
 		<div className="rounded-lg p-6">
-		  <div className='font-bold bg-gradient-to-r from-cyan-500 to-cyan-500 bg-clip-text text-transparent text-4xl'>{props.title}</div>
-		  <div className='font-bold text-white text-lg'>
+		  <div className='font-bold bg-gradient-to-r from-cyan-500 to-cyan-500 bg-clip-text text-transparent sm:text-2xl text-4xl'>{props.title}</div>
+		  <div className='font-bold text-white sm:text-md lg:text-lg'>
 			<ul className="list-disc pl-6">
 			  {detailsList.map((detail, index) => (
 				<li key={index} className="text-white">{detail.trim()}</li>
